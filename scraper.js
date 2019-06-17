c1024a15f1633e031dadbc18f049f889f419042f
subhyde <ejohnson16@myseneca.ca>
2019-06-17 12:05:18 -0400

    Added some text logs to show progress fixed bug where program would randomly crash

--- scraper.js
+++ scraper.js
@@ -5,17 +5,20 @@ const rl = readline.createInterface({
     output: process.stdout,
 });
 
-    const fsExtra = require('fs-extra');
+    const fsExtra = require('fs-extra'); // used to delete images after zip
     const fs = require('fs');
     const puppeteer = require('puppeteer'); // used to grab images and autoscroll
     const zipFolder = require('zip-a-folder'); // used to zip files for download
 
 
 
-startscreen();
+startscreen(); //starts program
+
+
 function startscreen(){
     rl.question('Choose a selection:\nPress(1) Start scrape\nPress(2) Info\n',answer => {
         if(answer === '1'){
+
 
             getusername()
         }
@@ -37,10 +40,11 @@ function getusername(){
 
     rl.question('Please Enter the Username of the Instagram Account you would like to scrape:\n', answer => {
 
-    let instagramurl = "http://instagram.com/"+answer+"/";
+    let instagramurl = "https://instagram.com/"+answer+"/";
         console.log("scraping images...");
         rl.close();
         startscrape(instagramurl);
+
 
 });
 
@@ -50,27 +54,18 @@ function startscrape(instagramurl) {
 
 
 //launches the headless browser
-        let browser = await puppeteer.launch(({headless: true}));
+        let browser = await puppeteer.launch(({headless: false}));
         let page = await browser.newPage();
         let counter = 0;
 //finds all the files that contain jpeg in their url and saves them to the image folder
-        page.on('response', async (response) => {
 
-            const matches = /.*\.fbcdn*....$/.exec(response.url());
-            if (matches) {
-
-
-                const buffer = await response.buffer();
-                fs.writeFileSync(`images/image-${counter}.jpeg`, buffer, 'base64');
-
-                counter += 1; //adds +1 to each file saved
-
-            }
-        });
 
 
         await page.goto(instagramurl);
+
+
 //waits for page to fully load before scrolling and collecting the images
+
         await page.waitFor(1000);
         await autoScroll(page);
 
@@ -79,26 +74,44 @@ function startscrape(instagramurl) {
 
 
 
-        let data = await page.evaluate(() => {
+       await page.evaluate(() => {
+
             //doesnt currently work, finding a work around
-            /*
-            if (document.querySelector('.error-container')) {
-                console.log("sorry this is an invalid username");
-                getusername();
-            }*/
+
+           // if (document.querySelector('.error-container')) {
+           //     console.log("sorry this is an invalid username");
+             //   getusername();
+          //  }
+
 
             let username = document.querySelector('._7UhW9').innerText;
-            let name = document.querySelector('.rhpdm').innerText;
+
+               // let name = document.querySelector('.rhpdm').innerText;
+
 
 
             return {
-                username,
-                name
+                username
+
             }
 
         });
+        page.on('response', async (response) => {
 
-        console.log(data);
+            const matches = /.*\.fbcdn*....$/.exec(response.url());
+            if (matches) {
+
+
+                const buffer = await response.buffer();
+                fs.writeFileSync(`images/image-${counter}.jpeg`, buffer, 'base64'); //adds the jpeg extension to every image saved
+
+                counter += 1; //adds +1 to each file saved
+                console.log("image: " + counter + ' saved'); // logs every image saved
+
+            }
+        });
+
+
 
 
 //zips the files into the local folder
@@ -146,6 +159,7 @@ function startscrape(instagramurl) {
 //auto scroll function, set to 400 || this is less fast but will work on most internet speeds
 async function autoScroll(page){
     await page.evaluate(async () => {
+
         await new Promise((resolve) => {
             let totalHeight = 0;
             let distance = 100;
@@ -158,9 +172,10 @@ async function autoScroll(page){
                     clearInterval(timer);
                     resolve();
                 }
+
             }, 400);
+
         });
     });
+
 }
-
-
